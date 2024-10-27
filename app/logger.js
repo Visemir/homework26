7bfb3eb297e58cbb2826738d4d608295b1c87e60
Oleg.K <olegk@mypost.com>
2024-10-27 17:52:37 +0000

    add homework26

---
+++ app/logger.js
@@ -0,0 +1,25 @@
+const winston = require('winston');
+const fluentLogger = require('fluent-logger');
+
+const fluentTransport = fluentLogger.createFluentSender('js_app', {
+  host: process.env.FLUENT_HOST,
+  port: 24224,
+  timeout: 3.0
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [new winston.transports.Console()]
+});
+
+logger.on('data', (log) => {
+  fluentTransport.emit('winston', {
+    level: log.level,
+    message: log.message,
+    meta: log.meta || {},
+    timestamp: new Date(). toISOString()
+  });
+});
+
+module.exports = logger;
+
